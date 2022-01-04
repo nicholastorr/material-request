d87758b094f18320a85caae0e2452b928a54f0f2
nick <nick.torres213@gmail.com>
2022-01-04 15:35:51 +0000

    documenting req sheet

--- application/views/pages/request/v_request_sheet.php
+++ application/views/pages/request/v_request_sheet.php
@@ -14,12 +14,14 @@
 <div class="body">
     <div class="categories">
         <h2>Categories</h2>
-        <!---on click, pass data to model--->
+        <!---on click, add products that match clicked category to request sheet--->
         <?php foreach ($categories as $category): ?>
             <li id="<?php echo str_replace(' ', '-', $category['category']) . 1 ?>" style="font-size: 30px;"><?php echo ucfirst($category['category']) ?>
             <a style="margin-left: 20px; height: 15px;" href="/request/C_request/delete_category/<?php echo $category['category'] ?>"><i style="height: 15px; color: red;" class="fas fa-trash-alt"></i></a></li>
         <?php endforeach; ?>
 
+        <!--only admin can add new product-->
+        <!--Form to add new product-->
         <?php if (in_array('Administrator', $user['roles'])) { ?>
             <h3 id="add_product_button"><span class="glyphicon glyphicon-plus"></span>Add Product</h3>
         <?php
@@ -35,8 +37,9 @@
             </select>
                 <span style="display: none"><input type="textarea" id="this_wh" name="this_wh" value="<?= $warehouse['warehouse_code'] ?>"  style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></input></span>
         </form>
+        <!--end of add product form-->
 
-
+        <!--Form to add new category (only for admin)-->
             <h3 id="add_category_button"><span class="glyphicon glyphicon-plus"></span>Add Category</h3>
             <?php
                 $attrs = array('name' => 'new_category', 'class' => 'new_category', 'style' => 'display: none');
@@ -45,44 +48,48 @@
                 <input type="text" name="category" placeholder="Category" required />
                 <span style="display: none"><input type="textarea" id="this_wh" name="this_wh1" value="<?= $warehouse['warehouse_code'] ?>"  style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></input></span>
             </form>
+        <!--end of add category form-->
         <?php } ?>
 </div>
-
+            <!--submit button for product form-->
             <button type="button" style="display: none" class="btn btn-primary btn-lg"  onclick="add_product()" id="accept-count">submit</button>
+            <!--submit button for category form-->
             <button id="category-button" type="button" style="display: none" class="btn btn-primary btn-lg"  onclick="add_category()" id="accept-count">submit</button>
         <?php echo form_close(); ?>
 
+        <!--request form-->
         <?php
             $attributes = array('name' => 'count_form');
             echo form_open_multipart('request/C_request/get_request_sheet/box?wh=' .  strtolower($warehouse['warehouse_code']) , $attributes);
         ?>
         <div class="new-form">
-                <div class="form-info">
-                    <h2><span class="glyphicon glyphicon-asterisk"></span>Order, Comments, Qty Required -- Leave blank to omit SKU from request</h2>
-                    <h2><span class="glyphicon glyphicon-asterisk"></span>To adjust position, leave fields blank and input weight (descending order)</h2>
-                </div>
+            <div class="form-info">
+                <h2><span class="glyphicon glyphicon-asterisk"></span>Order, Comments, Qty Required -- Leave blank to omit SKU from request</h2>
+                <h2><span class="glyphicon glyphicon-asterisk"></span>To adjust position, leave fields blank and input weight (descending order)</h2>
+            </div>
             <div class="form-headings">
-            <h2>Sku</h2>
-            <h2>Order</h2>
-            <h2>Comments</h2>
-            <h2 class="smaller-head">Qty</h2>
-            <h2 class="smaller-head">Weight</h2>
+                <h2>Sku</h2>
+                <h2>Order</h2>
+                <h2>Comments</h2>
+                <h2 class="smaller-head">Qty</h2>
+                <h2 class="smaller-head">Weight</h2>
             </div>
-        <?php foreach($products as $product) { ?>
-            <div id="form" class="<?php echo str_replace(' ', '-', $product['category']) ?>"  style="display: flex; flex-direction: column; display: none">
-            <div class="form">
-                <a href="/request/C_request/delete_product/<?= $product['sku'] ?>"><span class="glyphicon glyphicon-remove"></span></a>
-                <span><input type="textarea" value="<?= $product['sku'] ?>" id="sku" name="sku[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
-                <span class="order"><input type="textarea" id="order" name="order[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
-                <span class="comments"><input type="textarea" id="comments" name="comments[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
-                <span><input type="textarea" id="good" name="qty[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
-                <span><input type="textarea" value="<?= $product['weight'] ?>" placeholder="<?= $product['weight'] ?>" id="good" class="weight" name="weight[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
-            </div>
-            </div>
-                <?php echo '<script>var myPhpVariable = "'. str_replace(' ', '-', $product['category']) . '";</script>'; ?>
-        <?php }; ?>
+                <!--display all selected products, starts with empty sheet-->    
+                <?php foreach($products as $product) { ?>
+                    <div id="form" class="<?php echo str_replace(' ', '-', $product['category']) ?>"  style="display: flex; flex-direction: column; display: none">
+                        <div class="form">
+                            <a href="/request/C_request/delete_product/<?= $product['sku'] ?>"><span class="glyphicon glyphicon-remove"></span></a>
+                            <span><input type="textarea" value="<?= $product['sku'] ?>" id="sku" name="sku[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
+                            <span class="order"><input type="textarea" id="order" name="order[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
+                            <span class="comments"><input type="textarea" id="comments" name="comments[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
+                            <span><input type="textarea" id="good" name="qty[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
+                            <span><input type="textarea" value="<?= $product['weight'] ?>" placeholder="<?= $product['weight'] ?>" id="good" class="weight" name="weight[]" style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></span>
+                        </div>
+                    </div>
+                    <?php echo '<script>var myPhpVariable = "'. str_replace(' ', '-', $product['category']) . '";</script>'; ?>
+                <?php }; ?>
         </div>
-
+        <!---submit button for request form-->
         <div style="display: none"><input type="textarea" id="warehouse" name="warehouse[]" value="<?= $warehouse['warehouse_code'] ?>"  style="font-size: 30px; background-color: #DCDCDC; height: 80%; margin-right: 15px"></input></div>
         <button type="button" class="btn btn-primary btn-lg"  onclick="myFunc()" id="count">submit</button>
 
